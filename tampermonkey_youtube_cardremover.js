e5efe08fcf4592a504322709aed5e83826b206df
Andrew Peng <pengc99@gmail.com>
2021-06-27 16:50:10 -0500

    Update tampermonkey_youtube_cardremover.js

--- tampermonkey_youtube_cardremover.js
+++ tampermonkey_youtube_cardremover.js
@@ -4,6 +4,7 @@
 // @description  Remove YouTube end video cards overlay on top of videos
 // @author       pengc99
 // @namespace    https://github.com/pengc99/tampermonkey_youtube_cardremover
+// @downloadURL 
 // @require      https://cdn.jsdelivr.net/gh/CoeJoder/waitForKeyElements.js@v1.2/waitForKeyElements.js
 // @include      http://www.youtube.com/*
 // @include      https://www.youtube.com/*
@@ -16,7 +17,9 @@
 var cardElement = "ytp-ce-element";
 
 function removeCards () {
+    console.log("## Entered removeCards()");
     while (document.getElementsByClassName(cardElement).length != 0) {
+        console.log("## Removing " + cardElement);
         document.getElementsByClassName(cardElement)[0].remove();
     }
 }
