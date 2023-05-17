d5e73b89b4935f4b56f5f65e742781157b38ce9a
Andrew Peng <pengc99@gmail.com>
2023-05-17 11:41:43 -0500

    Update tampermonkey_youtube_cardremover.user.js
    Removed blank @grant line and added @homepage line

--- tampermonkey_youtube_cardremover.user.js
+++ tampermonkey_youtube_cardremover.user.js
@@ -1,18 +1,18 @@
 // ==UserScript==
 // @name         YouTube - CardRemover
-// @version      1.33
+// @version      1.34
 // @description  Remove YouTube end video cards overlay on top of videos
 // @author       pengc99
 // @license	 GNU GPL v3.0
 // @namespace    https://github.com/pengc99/tampermonkey_youtube_cardremover
 // @updateURL    https://raw.githubusercontent.com/pengc99/tampermonkey_youtube_cardremover/main/tampermonkey_youtube_cardremover.user.js
 // @downloadURL  https://raw.githubusercontent.com/pengc99/tampermonkey_youtube_cardremover/main/tampermonkey_youtube_cardremover.user.js
+// @homepage     https://github.com/pengc99/tampermonkey_youtube_cardremover
 // @include      http://www.youtube.com/*
 // @include      https://www.youtube.com/*
 // @include      http://youtube.com/*
 // @include      https://youtube.com/*
 // @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAABmJLR0QA/wD/AP+gvaeTAAABJElEQVRIie2WTYrCQBBGn4MTVypkMTDOicSNXkMzXtIZf0BFcKFr9QSJ+7joirRJRzTpRkE/KEi+dOqliuok8NarqQMcgNhy7IH2NfDeATSJnQ6qpMDxDV0pozPvwzEoV9UcP92Jssp08mEV3wuuAb/AFDhKTIAA8Mo8SDKBJv0AK/Kndgm0CuS9uqCWgg4AH6gDXWAj/gJz5YXBQy6rWwNf2vWmBh/YBM+0pOsceE/8iU1wJL4vMBO8IV5oExyKX5dzE9x3AZ6K39W8NDyQ47FNcJJ0gxokEzyJvk2wh9qnMbBFDVID1d5Au28OfNoEg3o5LLmsTo858F0g700LPNSWGqMmPQL+Ue01VWoNXFSZvE/3PXb9J5Kp+M8ha+Qw91tPqBNQe7BEabp3fAAAAABJRU5ErkJggg==
-// @grant
 // ==/UserScript==
 
 var cardElement = "ytp-ce-element";
